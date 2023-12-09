6e671c617398cd32d225e46150c2f2e46ca32b86
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-09 15:29:43 -0500

    add custom hook to fetch projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,12 @@
-import './fetchProjects';
+import useFetchProjects from './fetchProjects';
 
 const Projects = () => {
+  const { projects, loading } = useFetchProjects();
+
+  if (loading) {
+    return <div className="loading"></div>;
+  }
+
   return <div>Projects</div>;
 };
 export default Projects;
--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -1,10 +1,39 @@
 import { createClient } from 'contentful';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const client = createClient({
   space: '4yts2s6194dp',
   accessToken: import.meta.env.VITE_ACCESS_TOKEN,
 });
 
-client.getEntries({ content_type: 'projects' }).then((response) => {
-  console.log(response);
-});
+const useFetchProjects = () => {
+  const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState([]);
+
+  const getData = async () => {
+    try {
+      const response = await client.getEntries({ content_type: 'projects' });
+      const projectsArray = response.items.map((item) => {
+        const { url, title, image } = item.fields;
+        const id = item.sys.id;
+        const img = image?.fields?.file?.url;
+        return { title, img, url, id };
+      });
+
+      setProjects(projectsArray);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return { projects, loading };
+};
+
+export default useFetchProjects;
